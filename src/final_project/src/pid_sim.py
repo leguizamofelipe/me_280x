7282bdc9328289b952bcd2e7af9ef2296c744a99
Felipe Leguizamo <leguizamofelipe@outlook.com>
2021-11-22 23:11:36 -0600

    Added tunes for angular motion

--- src/final_project/src/pid_sim.py
+++ src/final_project/src/pid_sim.py
@@ -1,35 +1,34 @@
-#! /usr/bin/env python2.7 
-# we need 2.7 to import tf
-
-import rospy # import rospy, a ROS-python wrapper 
-from turtlesim.msg import Pose # import turtlesim message package. Pose is the daya type used to store x,y coordinates
+# import rospy # import rospy, a ROS-python wrapper 
+# from turtlesim.msg import Pose # import turtlesim message package. Pose is the daya type used to store x,y coordinates
 from math import pow, atan2, sqrt, pi # import auxiliary math functions
-from sensor_msgs.msg import LaserScan # import sensor message package to utilize the Lidar sensor
-from geometry_msgs.msg import Twist  # import geometry message package. Twist is the data type used to describe linear and angular velocities
-from nav_msgs.msg import Odometry # import navigation message package. Odometry is the data type to extract turtlebot's position in quaternions
-import tf
-from tf.transformations import euler_from_quaternion #import transformation package, which allows us to convert from quaternions to eulerian coordinates
+# from sensor_msgs.msg import LaserScan # import sensor message package to utilize the Lidar sensor
+# from geometry_msgs.msg import Twist  # import geometry message package. Twist is the data type used to describe linear and angular velocities
+# from nav_msgs.msg import Odometry # import navigation message package. Odometry is the data type to extract turtlebot's position in quaternions
+# import tf
+# from tf.transformations import euler_from_quaternion #import transformation package, which allows us to convert from quaternions to eulerian coordinates
+import time
+
+class Pose():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        pass
+
+class Vel():
+    def __init__(self):
+        self.x = 0
+        self.z = 0
+        pass
 
 class TurtleBot:
 
-    def __init__(self):
-        # Creates a node with name 'turtlebot_controller' and make sure it is a unique node (using anonymous=True).
-        rospy.init_node('turtlebot_controller', anonymous=True)
-
-        # Publisher which will publish to the topic 'robot3/cmd_vel'.
-        self.velocity_publisher = rospy.Publisher('robot3/cmd_vel', Twist, queue_size=10)
-
-        # A subscriber to the topic '/odom'. self.update_pose is called when a message of type Pose is received.
-        self.pose_subscriber = rospy.Subscriber('robot3/odom', Odometry, self.update_pose)
-        self.laser_suscriber = rospy.Subscriber('robot3/scan', LaserScan, self.update_scan)
-
-        # Create a pose object attributed to turtlebot
-        self.odom = Odometry
+    def __init__(self, init_x, init_y):
         # Set the rate at which we publish/suscribe in Hz
-        self.rate = rospy.Rate(10)
-        # We need a short pause to allow self.pose to suscribe from the topic and accurate display turtlebots pose
-        rospy.sleep(0.5)
-        print('Initiliazing at x:{}, y:{}'.format(self.pos_x, self.pos_y))
+        self.rate = 0.1
+
+        self.position = Pose()
+
+        print('Initiliazing at x:{}, y:{}'.format(self.position.x, self.position.x))
 
         self.distance_error = 0
         self.previous_distance_error = 0 
@@ -39,6 +38,9 @@ class TurtleBot:
         self.previous_angular_error = 0
         self.sum_angular_error = 0
 
+        self.velocity = Vel()
+    
+    '''
     def update_pose(self, data):
         """Callback function that is called when a new message of type Pose is received by the pose_subscriber."""
         self.odom = data
@@ -50,11 +52,7 @@ class TurtleBot:
         # we only use yaw angle since in this case, turtlebot is constrained to rotation around the z-axis
         self.theta = self.yaw
 
-    def update_scan(self, data):
-        """Callback function that is called when a new message of type LaserScan is received by the laser_subscriber."""
-
-        # updates distance to any obstacle in front of turtlebot
-        self.front_laser = data.ranges[0]
+    '''
 
     def euclidean_distance(self, goal_pose):
         """Euclidean distance between current pose and the goal."""
@@ -87,9 +85,9 @@ class TurtleBot:
 
     def move2goal(self):
         """Moves the turtlebot to the goal."""
-
         # Creates a pose object
         goal_pose = Pose()
+        vel = Vel()
 
         # Get the input from the user.
         goal_pose.x = float(input("Set your x goal: "))
@@ -98,39 +96,27 @@ class TurtleBot:
         k_i = float(input("Set constant for K_i: "))
         k_d = float(input("Set constant for K_d: "))
 
-        k_p_angular = float(input("Set constant for K_p angular: "))
-        k_i_angular = float(input("Set constant for K_i angular: "))
-        k_d_angular = float(input("Set constant for K_d angular: "))
-
         # Insert a number slightly greater than 0 (e.g. 0.01).
         distance_tolerance = float(input("Set your tolerance for goal: "))
 
-        # Instantiate Twist object to send mesg to turtlebot
-        vel_msg = Twist()
-
         # feedback loop to keep sending control signal while distance > tolerance
         while self.euclidean_distance(goal_pose) >= distance_tolerance:
+            # Linear velocity in the x-axis.
+            vel.x = self.linear_vel(goal_pose, k_p, k_i, k_d)
 
-            if self.front_laser < 0.75:
-                print('Obstacle detected in front, modify code below to avoid collision')
-                vel_msg.linear.x = self.linear_vel(goal_pose, k_p, k_i, k_d)
-                vel_msg.angular.z = self.angular_vel(goal_pose, k_p_angular, k_i_angular, k_d_angular)
-            else:
-                # Linear velocity in the x-axis.
-                vel_msg.linear.x = self.linear_vel(goal_pose, k_p, k_i, k_d)
+            # Angular velocity in the z-axis.
+            vel.z = self.angular_vel(goal_pose, k_p, k_i, k_d)
 
-                # Angular velocity in the z-axis.
-                vel_msg.angular.z = self.angular_vel(goal_pose, k_p_angular, k_i_angular, k_d_angular)
+            # Find robot position after each timestep
 
-            # Publishing our vel_msg
-            self.velocity_publisher.publish(vel_msg)
+            self.position = 
 
             # Publish at the desired rate.
-            self.rate.sleep()
+            time.sleep(self.rate)
 
         # Stopping our robot after destination is reached
-        vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
+        vel.x = 0
+        vel.z = 0
         self.velocity_publisher.publish(vel_msg)
 
         print('Arrived at location x:{}, y:{}'.format(self.pos_x, self.pos_y))
@@ -139,5 +125,5 @@ if __name__ == '__main__':
     try:
         x = TurtleBot()
         x.move2goal()
-    except rospy.ROSInterruptException:
+    except:
         pass
