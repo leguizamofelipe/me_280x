8213c5213fe53dc5ae08353a656aba1e3e145a63
Felipe Leguizamo <leguizamofelipe@outlook.com>
2021-11-23 22:09:49 -0600

    Batch launch script

--- src/final_project/src/turtlebot_cmd.py
+++ src/final_project/src/turtlebot_cmd.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # import necessary packages below
+from final_project.src.generic_robot import TurtleBot
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
@@ -11,45 +12,39 @@ import time
 import os
 #import pandas as pd
 
-from turtlebot_PID_go2goal import *
-
 if __name__ == '__main__':
-	baseline = [8,8,1,0,0,2,0,0,0.25]
+    robot_1 = TurtleBot(1)
+    robot_2 = TurtleBot(2)
+    robot_3 = TurtleBot(3)
+    robot_4 = TurtleBot(4)
 
-	results_dict = {}
+    robot_1.move2goal(5, 5)
+    robot_2.move2goal(5, 5)
+    robot_3.move2goal(5, 5)
+    robot_4.move2goal(5, 5)
 
-	runs = [
-		[8 , 8 , 10 , 0.5 , 0.2 , 15 , 1.2 , 0 , 0.25],
-	]
-	
-	run_times = []
-	vals_list = []
-	
-	for val in runs:
-		os.environ['TURTLEBOT3_MODEL'] = "waffle_pi"
+    '''
+    for val in runs:
 
-		time.sleep(1)
-		
-		process2 = subprocess.Popen(['roslaunch', 'turtlebot3_gazebo', 'turtlebot3_empty_world.launch'])	
-		time.sleep(5)
+        process2 = subprocess.Popen(['roslaunch', 'turtlebot3_gazebo', 'turtlebot3_empty_world.launch'])	
+        time.sleep(5)
 
-		x = TurtleBot()
-		run_time = x.move2goal(val, 3)
-		
-		run_times.append(run_time)	
+        x = TurtleBot()
+        run_time = x.move2goal(val, 3)
+        
+        run_times.append(run_time)	
 
-		print("Complete in:")
-		print(run_time)
-		process2.kill()
-		
-		vals_list.append(val)
-	
-	results_dict = {"Vals":vals_list,
-			"Runtime":run_times}
+        print("Complete in:")
+        print(run_time)
+        process2.kill()
+        
+        vals_list.append(val)
+    
+    results_dict = {"Vals":vals_list,
+            "Runtime":run_times}
 
-	#results_dict = pd.DataFrame(results_dict)
+    print(run_times)
+    print(results_dict)
 
-	#results_dict.to_csv('output/results_' + str(round(time.time()))+ '.csv', index = False)
-	
-	print(run_times)
-	print(results_dict)
+    '''
+    
